4eff84b6bc44a5e02f55bb2a92fb47cfebace963
tea <teaganschultz@gmail.com>
2020-09-22 13:24:53 -0400

    added textfield to form state

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import React from 'react';
-import PortfolioForm from './components/PortfolioForm';
+import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import PortfolioForm from './components/portfolioForm';
 
-function App() {
+/*export default function App() {
   return (
     <div className="App">
       <PortfolioForm></PortfolioForm>
     </div>
   );
-}
+}*/
 
-export default App;
+//TODO: add header!
+
+export default function App() {
+  return (
+      <Container maxWidth="sm" align="center">
+        <Typography variant="h5" component="h1">
+          Data submission protoype
+        </Typography>
+        <PortfolioForm />
+      </Container>
+  );
+}
