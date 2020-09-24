32ba91b12a5b9037f875ef5caaa9a34cf4929c62
tea <teaganschultz@gmail.com>
2020-09-24 13:51:27 -0400

    styling and select component refactor

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Avatar from '@material-ui/core/Avatar';
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 import FormControl from '@material-ui/core/FormControl';
-import InputLabel from '@material-ui/core/InputLabel';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import Box from '@material-ui/core/Box';
-import Link from '@material-ui/core/Link';
 import Typography from '@material-ui/core/Typography';
-
-import PortfolioSelect from './PortfolioSelect';
 import { LibraryAdd } from '@material-ui/icons';
-
+import PortfolioSelect from './components/PortfolioSelect';
+import useObjectState from './hooks/useObjectState';
 
 const portfolioOptions = ['pension', 'endowment'];
 const level1Options = ['equity', 'fixed income', 'cash'];
@@ -27,11 +23,6 @@ function Copyright() {
   return (
     <Typography variant="body2" color="textSecondary" align="center">
       {'Copyright © '}
-      {/*
-      <Link color="inherit" href="https://material-ui.com/">
-        Your Website
-      </Link>{' '}
-      */}
       {new Date().getFullYear()}
       {'.'}
     </Typography>
@@ -39,52 +30,61 @@ function Copyright() {
 }
 
 const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
   paper: {
-    marginTop: theme.spacing(16),
+    marginTop: theme.spacing(15),
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'center',
   },
   avatar: {
-    marginTop: theme.spacing(6),
+    marginTop: theme.spacing(2),
     //backgroundColor: theme.palette.secondary.main,
   },
   form: {
     width: '100%',
-    marginTop: theme.spacing(3),
+    margin: theme.spacing(1),
   },
   formControl: {
-    margin: theme.spacing(2),
-    width: 160
+    width: '200px',
+    margin: theme.spacing(1),
   },
   textField: {
-
+    width: '200px',
+    margin: theme.spacing(1),
   },
   submit: {
-    margin: theme.spacing(3, 0, 2),
+    marginTop: theme.spacing(1),
+    marginBottom: theme.spacing(3),
   },
 }));
 
 export default function App() {
-  const [formState, _setFormState] = useState({
+  const [formState, setFormState] = useObjectState({
     portfolio: '',
     level1: '',
     level2: '',
     fund: '',
   });
 
-  const setFormState = fieldAndValue => _setFormState({...formState, ...fieldAndValue }); 
   const handleSelect = val => setFormState(val);
   const handleTyping = ({ target }) => setFormState({ fund: target.value });
-  const handleSubmit = () => {
-    //proc alert that displays json object, for demo purposes
-    console.log(formState);
+  const handleSubmit = (e) => {
+    window.alert(JSON.stringify({
+      portfolio: formState.portfolio,
+      level1: formState.level1,
+      level2: formState.level2,
+      fund: formState.fund,
+    }));
+    e.preventDefault();
   };
   
   const classes = useStyles();
   
   return (
-    <Container component="main" maxWidth="xs">
+    <Container component="main" maxWidth="xs" className={classes.root}>
       <AppBar position="absolute" color="default" className={classes.appBar}>
         <Toolbar>
           <Typography variant="h6" color="inherit" noWrap>
@@ -103,44 +103,49 @@ export default function App() {
         
         <form className={classes.form} onSubmit={handleSubmit}>
           <Grid container 
-            spacing={2}
+            spacing={1}
             justify="center"
             direction="column"
             alignItems="center"
           >
-            <Grid item xs={12}>
-              <FormControl className={classes.formControl}>
-                <InputLabel>Portfolio</InputLabel>
+            
+            <Grid item>
+              <FormControl variant="outlined" className={classes.formControl}>
                 <PortfolioSelect
                   field="portfolio" 
                   options={portfolioOptions} 
                   value={formState.portfolio} 
+                  label="Portfolio"
+                  onChange={handleSelect}
+                />
+              </FormControl>
+            </Grid>
+
+            <Grid item>
+              <FormControl variant="outlined" className={classes.formControl}>
+                <PortfolioSelect 
+                  field="level1" 
+                  options={level1Options} 
+                  value={formState.level1}
+                  label="Level I" 
+                  onChange={handleSelect}
+                />
+              </FormControl>
+            </Grid>
+
+            <Grid item>
+              <FormControl variant="outlined" className={classes.formControl}>
+                <PortfolioSelect 
+                  field="level2"
+                  options={level2Options}
+                  value={formState.level2}
+                  label="Level II"
                   onChange={handleSelect} 
                 />
               </FormControl>
             </Grid>
 
-            <Grid item xs={12}>
-              <FormControl className={classes.formControl}>
-                <InputLabel>Level I</InputLabel>
-                <PortfolioSelect 
-                  field="level1" 
-                  options={level1Options} 
-                  value={formState.level1} 
-                  onChange={handleSelect} 
-                />
-              </FormControl>
-            </Grid>
-              <FormControl className={classes.formControl}>
-                <InputLabel>Level II</InputLabel>
-                <PortfolioSelect 
-                  field="level2"
-                  options={level2Options}
-                  value={formState.level2}
-                  onChange={handleSelect} 
-                />
-              </FormControl>
-            <Grid item xs={12} sm={6}>
+            <Grid item>
               <TextField 
                 variant="outlined"
                 required
@@ -150,15 +155,18 @@ export default function App() {
                 className={classes.textField}
               />
             </Grid>
+
+            <Grid item>
+              <Button 
+                variant="contained" 
+                type="submit"
+                color="primary"
+                className={classes.submit}
+              >
+                Submit
+              </Button>
+            </Grid>
           </Grid>
-          <Button 
-            variant="contained" 
-            type="submit"
-            color="primary"
-            className={classes.submit}
-          >
-            Submit
-          </Button>
         </form>
       </Paper>
       <Box mt={5}>
@@ -167,12 +175,3 @@ export default function App() {
     </Container>
   );
 }
-
-/*
-TODO: 
-  -TextField spacing and style
-  -replace div with Paper
-  -Style Paper a bit nicer
-  -Investigate FormControl usage wrt Select elements
-  -avatar
-*/
